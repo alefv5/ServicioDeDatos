a13f0a2439a6351cbfdd25d735715ae01291183a
Diego Orlando Mejia Salazar <dmejia@pros.com>
2021-09-28 03:02:38 -0400

    fixed useless try-catch in crearTopico

--- src/app/services/topicoService.js
+++ src/app/services/topicoService.js
@@ -11,15 +11,12 @@ const topicoService = {
         return { 'respuesta': topico };
     },
     crearTopico: async(request, response) => {
-        try {
-            const nuevoTopico = await Topico.create(request.body);
-            return { message: "El Topico fue creado exitosamente", result: nuevoTopico};
-        } catch (error) {
-            throw error;
-        }
+        const nuevoTopico = await Topico.create(request.body);
+        return { message: "El Topico fue creado exitosamente", result: nuevoTopico};
+
     },
     editarTopico: async(request, response) => {
-        const topicoEditado = await Topico.update(request.body, { where: { id: request.params.id } });
+        await Topico.update(request.body, { where: { id: request.params.id } });
         let topico = await Topico.findByPk(request.params.id);
         return { message: "El Topico fue editado exitosamente", Topico: topico };
     },
